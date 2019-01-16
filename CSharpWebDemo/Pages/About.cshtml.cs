b9f83d4aa1c28de079974e212f5367215c955984
Jianjun <jianjlv@microsoft.com>
2019-01-16 22:59:05 +0800

    first commit

---
+++ CSharpWebDemo/Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSharpWebDemo.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "What's my message?";
+        }
+    }
+}
