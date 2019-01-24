6c4324ce83163801b5f73d636fae24f8296c4379
Jianjun <jianjlv@microsoft.com>
2019-01-24 14:29:51 +0800

    add new web api 2 controller

---
+++ CSharpMVCWebAPIApplication/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CSharpMVCWebAPIApplication.Controllers
+{
+    [RoutePrefix("api/test")]
+    public class TestController : ApiController
+    {
+
+        public string GetTestsData()
+        {
+            return "all data";
+        }
+        [Route("get/{id}")]
+        public string GetDataById(int id)
+        {
+            return "data_" + id;
+        }
+        [Route("get/{id}")]
+        public string GetDataByCategory(string category)
+        {
+            return "category_" + category;
+        }
+    }
+}
