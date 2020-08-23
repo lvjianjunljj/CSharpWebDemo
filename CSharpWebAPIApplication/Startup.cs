500ce6d76fdede2c21b7c75d1cd0944e9f604810
Jianjun Lv <jianjlv@microsoft.com>
2020-08-24 02:01:05 +0800

    Remove useless line

--- CSharpWebAPIApplication/Startup.cs
+++ CSharpWebAPIApplication/Startup.cs
@@ -1,16 +1,14 @@
 ﻿namespace CSharpWebAPIApplication
 {
-    using Microsoft.IdentityModel.Protocols;
     using Microsoft.IdentityModel.Tokens;
     using Microsoft.Owin.Security.ActiveDirectory;
-    using Newtonsoft.Json.Linq;
     using Owin;
-    using System.Net.Http;
     using System.Web.Configuration;
     public partial class Startup
     {
         // The necessary class for Owin to implement the auth based on AAD
         // doc link: https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-v1-dotnet-webapi
+        // https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-v2-aspnet-webapp
         // https://blogs.msdn.microsoft.com/practice-sharing/2016/03/22/office-365-dev-how-to-validate-the-access-token-issued-by-microsoft-azure-ad/
         public void Configuration(IAppBuilder app)
         {
