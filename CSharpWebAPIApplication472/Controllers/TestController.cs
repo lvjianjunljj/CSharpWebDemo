85a37e2334bd1882d770f1fa386f97b2e214c5c5
Jianjun Lv <jianjlv@microsoft.com>
2021-06-07 13:06:24 +0900

    First commit for CSharpWebAPIApplication472

---
+++ CSharpWebAPIApplication472/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpWebAPIApplication472.Controllers
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    [RoutePrefix("api/tests")]
+    public class TestController : ApiController
+    {
+        [Route("")]
+        public HttpResponseMessage Get()
+        {
+            return this.Request.CreateResponse(HttpStatusCode.OK, new string[] { "1", "2" });
+        }
+    }
+}
---
+++ CSharpWebAPIApplication472/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSharpWebAPIApplication472.WebApiApplication" Language="C#" %>
